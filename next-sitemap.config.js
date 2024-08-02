352d812467523edbeaa77e906968a4bc56e6bcdd
canto1380 <atapenalba16@gmail.com>
2024-08-01 21:40:28 -0300

    correccion sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: 'https://jldistribuciones.com.ar', // Reemplaza con el dominio de tu sitio web
+  siteUrl: 'https://jldistribuciones.com', // Reemplaza con el dominio de tu sitio web
   changefreq: 'daily',
   sitemapSize: 5000, // Divide en archivos de 5000 URLs cada uno
   outDir: './public',
